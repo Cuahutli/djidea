df440b81489c66c4ba0b0e74092cbf9d420eda54
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-03-10 16:30:05 -0700

    New page view

--- ideas/urls.py
+++ ideas/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from pages.views import HomeView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name="index"),
 ]
