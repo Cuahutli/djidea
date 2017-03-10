e91bb1ed9050e143ceb9ce0e409bddb98fdf5390
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-03-10 14:42:00 -0700

    added lines config email, without custom domain

--- ideas/settings/production.py
+++ ideas/settings/production.py
@@ -30,6 +30,17 @@ DEBUG = False
 ALLOWED_HOSTS = ['curideas.herokuapp.com']
 
 
+#MAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'youremail@gmail.com' #my gmail username
+#EMAIL_HOST_PASSWORD = 'yourpassword' #my gmail password
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#DEFAULT_FROM_EMAIL = "Justin <hungrypy@gmail.com>"
+#
+#
+#ADMINS = [('Justin', EMAIL_HOST_USER)]
+#MANAGERS = ADMINS
+
 # Application definition
 
 INSTALLED_APPS = [
